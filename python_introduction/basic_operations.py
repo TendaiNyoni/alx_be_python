349519ec512015a1e025d0bd7546b4207d2eaaf8
TendaiNyoni <tnyoni265@gmail.com>
2025-09-16 05:24:41 -0700

    python_intro/basic_op

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,4 @@
-#Assign values
-number1 = 10
-number2 = 5
-
-#Perform operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
-
-#Print results
-print(Addition of 10 and 5 is 15)
-print(Subtraction of 10 and 5 is 5)
-print(Multiplication of 10 and 5 is 50)
+Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
 
